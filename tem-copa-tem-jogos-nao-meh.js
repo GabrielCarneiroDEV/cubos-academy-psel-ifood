d572ab3494b113c9b2ea275456c0d12a8f4610fc
Gabriel Carneiro <gabriel29music@gmail.com>
2021-06-08 17:37:15 -0300

    Desafio 7 - Tem copa? Tem jogos? Não? MEH

---
+++ tem-copa-tem-jogos-nao-meh.js
@@ -0,0 +1,13 @@
+function solucao(ano) {
+    if ((ano-2021) % 4 == 3){
+        console.log("JOGOS")
+        
+    }else if ((ano- 2021) % 2 == 1){
+        console.log("COPA")
+        
+    }else{
+        console.log("MEH")
+    }
+     
+     
+ }
